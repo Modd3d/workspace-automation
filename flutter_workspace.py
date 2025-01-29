fa5853a5817fded884cd8ec82e0c879efb06c967
Modd3d <69765315+Modd3d@users.noreply.github.com>
2025-01-29 18:31:30 -0500

    whats check_output?

--- flutter_workspace.py
+++ flutter_workspace.py
@@ -2263,8 +2263,8 @@ def install_minimum_runtime_deps():
 
         if os_release_id == 'ubuntu':
             cmd = ['sudo', 'apt', 'update', '-y']
-            subprocess.check_output(cmd)
-            
+            subprocess.call(cmd)
+
             packages = 'git git-lfs curl python3-pycurl python3-pip libcurl4-openssl-dev libssl-dev libgtk-3-dev python*-venv python3-toml python3-dotenv python3-pip python3-dev build-essential libcurl4-openssl-dev'
             cmd = ["sudo", "apt-get", "install", "-y", packages]
             subprocess.call(cmd)
